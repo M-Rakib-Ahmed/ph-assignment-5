e9b177ee75a8e4c5ba57c537b4bb8cc2c7532c67
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-02-23 16:26:49 +0600

    second file added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('#donate-now').addEventListener('click',()=>{
    
     const inputDonate = parseFloat(document.querySelector('#danate-input').value);
@@ -13,11 +15,46 @@ document.querySelector('#donate-now').addEventListener('click',()=>{
     document.querySelector('#pay-donate').innerText = succuessDonate;
     console.log(succuessDonate);
     
-    
-    
-    
-    
-         
-    
-    
 })
+
+
+
+// second functions start 
+document.querySelector('#donate-now1').addEventListener('click', () => {
+
+    const inputDonate = parseFloat(document.querySelector('#danate-input1').value);
+    const donateMoney = parseFloat(document.querySelector('#donate-money').innerText);
+    console.log(donateMoney);
+
+    const totalDoations = donateMoney - inputDonate;
+    console.log(totalDoations);
+    const payDonate = document.querySelector('#donate-money').innerText = totalDoations;
+
+    const Donate = parseFloat(document.querySelector('#pay-donate1').innerText);
+    const succuessDonate = inputDonate + Donate;
+    document.querySelector('#pay-donate1').innerText = succuessDonate;
+    console.log(succuessDonate);
+
+})
+
+// second functions end
+// third functions start 
+document.querySelector('#donate-now2').addEventListener('click', () => {
+
+    const inputDonate = parseFloat(document.querySelector('#danate-input2').value);
+    const donateMoney = parseFloat(document.querySelector('#donate-money').innerText);
+    console.log(donateMoney);
+
+    const totalDoations = donateMoney - inputDonate;
+    console.log(totalDoations);
+    const payDonate = document.querySelector('#donate-money').innerText = totalDoations;
+
+    const Donate = parseFloat(document.querySelector('#pay-donate2').innerText);
+    const succuessDonate = inputDonate + Donate;
+    document.querySelector('#pay-donate2').innerText = succuessDonate;
+    console.log(succuessDonate);
+
+})
+
+// third functions end
+
